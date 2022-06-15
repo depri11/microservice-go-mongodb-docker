831359c1a3bfa859509f486c083f2269f3f48edd
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-06-16 01:21:43 +0700

    add tag json in mode user

--- users/src/model/model.go
+++ users/src/model/model.go
@@ -3,7 +3,7 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	FirstName string             `bson:"firstname,omitempty"`
-	LastName  string             `bson:"lastname,omitempty"`
+	ID        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	FirstName string             `json:"firstname,omitempty" bson:"firstname"`
+	LastName  string             `json:"lastname,omitempty" bson:"lastname"`
 }
